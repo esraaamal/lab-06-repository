c020b76fef9bfd7ced19b85d9b65194fd75efb71
esraaamal <ma.esraa@hotmail.com>
2020-04-05 16:45:53 +0300

    edit

--- server.js
+++ server.js
@@ -5,14 +5,18 @@ const express =require('express');
 
 const server =express();
 
+const cors = require('cors');
+
+require('dotenv').config();
 
 
 
 const PORT = process.env.PORT ||3000;
 
+server.use(cors());
+
 //http://localhost:3000/test
 
-server.use(express.static('./data/geo.json'));
 
 server.get('/test',(request,response)=>{
 
@@ -21,6 +25,54 @@ server.get('/test',(request,response)=>{
     });
 
 //to test the server
+
+server.get('/location',(request,response)=>{
+
+    // response.send('youhhhhhhhhhhhhh are great Esraa');
+    const geo1 = require('./data/geo.json');
+    const city =request.query.city;
+    const loacData =new Location(city ,geo1);
+    response.send(loacData);
+
+
+    });
+
+
+//build a constructor for location
+
+function Location(city ,geo1){
+    this.search_query = city;
+    this.formatted_query = geo1[0].display_name;
+    this.latitude = geo1[0].lat;
+    this.longitude =geo1[0].lon;
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 server.listen(PORT ,() =>{
 
