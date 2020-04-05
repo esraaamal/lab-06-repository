453003b3ce3d0a23b241c2417a6aa4c077519dfc
esraaamal <ma.esraa@hotmail.com>
2020-04-05 18:40:57 +0300

    weather

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 `use strict`;
 
 
-const express =require('express');
+const express = require('express');
 
-const server =express();
+const server = express();
 
 const cors = require('cors');
 
@@ -11,42 +11,60 @@ require('dotenv').config();
 
 
 
-const PORT = process.env.PORT ||3000;
+const PORT = process.env.PORT || 3000;
 
 server.use(cors());
 
 //http://localhost:3000/test
 
 
-server.get('/test',(request,response)=>{
+server.get('/test', (request, response) => {
 
     response.send('youhhhhhhhhhhhhh are great Esraa');
-    
-    });
+
+});
 
 //to test the server
 
-server.get('/location',(request,response)=>{
+server.get('/location', (request, response) => {
 
     // response.send('youhhhhhhhhhhhhh are great Esraa');
     const geo1 = require('./data/geo.json');
-    const city =request.query.city;
-    const loacData =new Location(city ,geo1);
+    const city = request.query.city;
+    const loacData = new Location(city, geo1);
     response.send(loacData);
 
 
-    });
+});
 
 
 //build a constructor for location
 
-function Location(city ,geo1){
+function Location(city, geo1) {
     this.search_query = city;
     this.formatted_query = geo1[0].display_name;
     this.latitude = geo1[0].lat;
-    this.longitude =geo1[0].lon;
+    this.longitude = geo1[0].lon;
+
+}
 
 
+
+server.get('/weather', (request, response) => {
+let pushArr =[];
+    const weatherObj = require('./data/weather.json');
+    const city1 = request.query.city;
+    for (let s=0;s<weatherObj.data.length;s++){
+       pushArr.push(new Weather(weatherObj,s));
+      }
+    response.send(pushArr);
+
+
+});
+
+function Weather(weatherObj ,s) {
+    this.forecast = weatherObj.data[s].weather.description;
+    this.time = weatherObj.data[s].valid_date;
 
 }
 
@@ -54,6 +72,9 @@ function Location(city ,geo1){
 
 
 
+server.use('*', (req, res) => {
+    res.status(404).send('NOT FOUND');
+});
 
 
 
@@ -63,20 +84,9 @@ function Location(city ,geo1){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-server.listen(PORT ,() =>{
+server.listen(PORT, () => {
 
 
     console.log('listening on port', PORT);
-    
-    });
+
+});
