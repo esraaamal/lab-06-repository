135ab6e83ca90ba1c1a61c244e592db4b34e4ac0
esraaamal <ma.esraa@hotmail.com>
2020-04-06 15:43:15 +0300

    f

--- server.js
+++ server.js
@@ -1,84 +1,73 @@
 `use strict`;
 
 
-const express = require('express');
-
-const server = express();
-
-const cors = require('cors');
-
+// Load Environment Variables from the .env file
 require('dotenv').config();
+
+// Application Dependencies
+const express = require('express');
+const cors = require('cors');
+const superagent = require('superagent');
+
 
 
 
 const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(cors());
 
-server.use(cors());
+app.use(cors());
 
 //http://localhost:3000/test
 
-
-server.get('/test', (request, response) => {
-
-    response.send('youhhhhhhhhhhhhh are great Esraa');
-
-});
-
-//to test the server
-
-server.get('/location', (request, response) => {
-
-    // response.send('youhhhhhhhhhhhhh are great Esraa');
-    const geo1 = require('./data/geo.json');
-    const city = request.query.city;
-    const loacData = new Location(city, geo1);
-    response.send(loacData);
+app.get('/location',locationFunc);
+// app.get('/weather',weatherFunc);
 
 
-});
+
+function locationFunc(request,response){
+const city =request.query.city; //he will ask to put the city key with its value after the "?" in url
+getLocation(city)
+  .then(locationData=> response.status(200).json(locationData));
+}
 
 
-//build a constructor for location
+function getLocation(city){
+let key =process.env.GEOCODE_API_KEY;
+let url =`https://eu1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json`;
+return superagent.get(url)
+.then(data =>{
+    console.log(data);
+const locationData =new Location(city ,data.body);
+return locationData;
 
-function Location(city, geo1) {
+})
+}
+
+function Location(city, data) {
     this.search_query = city;
-    this.formatted_query = geo1[0].display_name;
-    this.latitude = geo1[0].lat;
-    this.longitude = geo1[0].lon;
-
-}
-
-
-
-server.get('/weather', (request, response) => {
-let pushArr =[];
-    const weatherObj = require('./data/weather.json');
-    const city1 = request.query.city;
-    for (let s=0;s<weatherObj.data.length;s++){
-       pushArr.push(new Weather(weatherObj,s));
-      }
-    response.send(pushArr);
-
-
-});
-
-function Weather(weatherObj ,s) {
-    this.forecast = weatherObj.data[s].weather.description;
-    this.time = weatherObj.data[s].valid_date;
-
-}
+    this.formatted_query = data[0].display_name;
+    this.latitude = data[0].lat;
+    this.longitude = data[0].lon;
+  }
+  
 
 
 
 
-// server.use((error, req, res) => {
-//     res.status(500).send(error);
-// })
 
 
-// server.use('*', (req, res) => {
-//     res.status(404).send('NOT FOUND');
-// });
+
+
+
+
+
+
+
+
+
+
+
 
 let errArr =[
 {
@@ -89,17 +78,14 @@ let errArr =[
 
 ];
 
-server.use('*',(request,response) => {
+app.use('*',(request,response) => {
     response.status(500).send(new CoverError());
 });
 
 function CoverError(){
     this.status =errArr[0].status;
     this.responseText=errArr[0].responseText ;
-    // this.error= error;
-    // status: 500,
-    // responseText: "Sorry, something went wrong",
-
+   
 }
 
 
@@ -108,7 +94,7 @@ function CoverError(){
 
 
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
 
 
     console.log('listening on port', PORT);
